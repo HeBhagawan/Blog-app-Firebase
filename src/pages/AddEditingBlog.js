fe5369dc4183340ccafaa9a706df3df7911ddc1a
HeBhagawan <manojgeeta487@gmail.com>
2024-02-22 09:41:40 +0530

    Implemented new features

--- src/pages/AddEditingBlog.js
+++ src/pages/AddEditingBlog.js
@@ -205,7 +205,13 @@ function AddEditingBlog({ user, setActive }) {
                   placeholder="Tags"
                   onChange={handleTags}
                 />
-                Hello
+                <button
+                  type="button"
+                  className="btn btn-primary ml-2"
+                  onClick={() => handleTags([...tags, ""])}
+                >
+                  Add
+                </button>
               </div>
               <div className="col-12 py-3">
                 <p className="trending">Is it trending blog?</p>
